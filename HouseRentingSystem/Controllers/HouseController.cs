85ee47c13f4843102ee0d2eef22a5e4594618531
GoshoPetrov <146186451+GoshoPetrov@users.noreply.github.com>
2026-03-23 18:16:06 +0200

    Refactor HomeController; add HouseController & views
    Removed Privacy functionality from HomeController, including the
    Privacy action and view. Updated _Layout.cshtml to replace the
    Privacy link with navigation to the new All Houses page and
    updated the app title to "Best House Rentingx."
    
    Introduced HouseController to manage house-related features,
    including hardcoded house data and actions for listing and
    viewing house details. Added a HouseViewModel class to
    represent house data. Created placeholder views for All Houses
    and Details actions.

---
+++ HouseRentingSystem/Controllers/HouseController.cs
@@ -0,0 +1,44 @@
+﻿using HouseRentingSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Controllers
+{
+    public class HouseController : Controller
+    {
+        private List<HouseViewModel> houses = new List<HouseViewModel>()
+        {
+            new HouseViewModel()
+            {
+                Id= 1,
+                Name = "Beach house",
+                Address = "Miami, Florida",
+                ImageUrl = @""
+
+            },
+            new HouseViewModel()
+            {
+                Id = 2,
+                Name = "Mountin house",
+                Address = "Rila, Bulgaria",
+                ImageUrl = @""
+            },
+            new HouseViewModel()
+            {
+                Id = 3,
+                Name = "Urban house",
+                Address = "Lulin, Sofia",
+                ImageUrl = @""
+            }
+        };
+        public IActionResult AllHouses()
+        {
+            return View(houses);
+        }
+
+        public IActionResult Details(int Id)
+        {
+            var d = houses.FirstOrDefault(h => h.Id == Id);
+            return View(d);
+        }
+    }
+}
